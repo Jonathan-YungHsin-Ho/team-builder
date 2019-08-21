ec5d3e5e5915a8a727262c6fc00d4eb963416a53
Jonathan Ho <johnnyho82@gmail.com>
2019-08-21 16:00:12 -0400

    Setup state and render team members

---
+++ team-builder/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { createGlobalStyle } from 'styled-components';
+// import './App.css';
+
+import data from './data';
+import Form from './components/Form';
+import Team from './components/Team';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    max-width: 100%;
+  }
+
+  html {
+    background-color: #C8102E;
+  }
+`;
+
+function App() {
+  const [players, setPlayer] = useState(data);
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <h1>1996-1997 Stanley Cup Champion Detroit Red Wings</h1>
+      <Form />
+      <Team players={players} />
+    </div>
+  );
+}
+
+export default App;
